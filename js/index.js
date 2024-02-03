2e8aecbc0fda34a6a6c71f68db2a66386651c0ad
Joaquin Villaruz <onea75120@gmail.com>
2024-02-03 11:55:50 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ let store = [];
 
 async function appInit() {
     const toDos = await getToDoData();
+    console.log(toDos);
 
     // Access the keys inside "toDos" then forEach key 
     // assign them a variable called "toDo" and log each of them
@@ -17,21 +18,21 @@ async function appInit() {
     // to get the values from the object we use the Object.values(obj) / Object.keys(obj) / Object.entries(obj)
 
     // The array we made stores values of the object in this variable
-    store = [...Object.values(toDos)];
+    // store = [...Object.values(toDos)];
 
-    // Create your own todo data
-    // find method
-    const key = "-NpB-eTM3CAL9ana07fx";
+    // // Create your own todo data
+    // // find method
+    // const key = "-NpB-eTM3CAL9ana07fx";
 
-    // Get the list of keys inside toDos
-    const keys = Object.keys(toDos);
-    const item = keys.find((item) => {
-        if (item === key) {
-            return item;
-        }
-    })
+    // // Get the list of keys inside toDos
+    // const keys = Object.keys(toDos);
+    // const item = keys.find((item) => {
+    //     if (item === key) {
+    //         return item;
+    //     }
+    // })
 
-    console.log(item);
+    // console.log(item);
 
 
 }
