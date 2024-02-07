1ac1d68ab6fd66a659e6fe1c486ee3fa77497ac4
Joaquin Villaruz <onea75120@gmail.com>
2024-02-06 18:43:53 -0800

    update

--- js/lib/firebase/api.js
+++ js/lib/firebase/api.js
@@ -1,35 +1,29 @@
-/*
-    Read Data from RTDB
-    
-*/
+/* 
+                Read Data from RTDB 
 
-import { ref, child, get } from 'firebase/database';
-
-import { db } from './../../config/firebase/firebaseInit';
-
-
-/*
-
-    connect to the to do list
-        reference ref
-         const dbRef = ref(db, "/todos");
-
-    asking for the data from the ref    path to a node in the JSON file...
-    get(ref) ===> data snapshot
-    get();
-
-    extracting the data
-    snapshot.val();
 
 */
 
-async function getToDoData(){
-    // references "todos.json";
-    const dbRef = ref(db, "/todos");
-    const dataSnapshot = await get(dbRef);
+import { ref, get } from 'firebase/database';
+import { db } from './config/firebase/firebaseInit';
 
-    // return a promise
-    return await dataSnapshot.val();
+/* 
+          1. connect to the to do list
+               reference  ref
+               const dbRef = ref(db, "/todos")
+
+          Asking for the data from that ref   path to a node in the JSON file...
+          get(ref)===> data snapshot
+
+          extracting the data
+          snapshot.val()
+
+*/
+
+async function getToDoData() {
+	const dbRef = ref(db, '/todos');
+	const dataSnapshot = await get(dbRef);
+	return await dataSnapshot.val();
 }
 
 export { getToDoData };
