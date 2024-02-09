347e9b1f84fdd7de8b556a8e4a5dbf1acb4c3e91
villaruzjoaquin <90798887+villaruzjoaquin@users.noreply.github.com>
2024-02-09 13:26:35 -0700

    removed index js from index.html

--- js/index.js
+++ js/index.js
@@ -5,4 +5,32 @@
 			document.querySelector('.display-list').insertAdjacentHTML("afterbegin", todo));
 			  
 */
+async function appInit() {
 
+	const appData = await getToDoData();
+	// Interactive Templating
+	const toDoItems = Object.values(appData).map((obj) => {
+		// inside the object, access the "todo" field
+		return toDoItemTemplate(obj.todo);
+	});
+
+
+
+	//render function jsx +++ data
+	// template function() passing the data =====> template string html string
+
+	// interactive templating
+	// container
+	// batch update ... to dom updates all at once
+	// const div = document.createElement('div');
+
+	// toDoItems.forEach((markup => {
+	// 	div.appendChild(markup);
+	// }))
+
+	// document.querySelector("main").append(div);
+
+
+}
+
+appInit();
