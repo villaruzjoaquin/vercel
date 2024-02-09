61ae03dfa6cc555ad3e7a113366e3ee88c81dc46
villaruzjoaquin <90798887+villaruzjoaquin@users.noreply.github.com>
2024-02-09 13:08:45 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,36 +5,4 @@
 			document.querySelector('.display-list').insertAdjacentHTML("afterbegin", todo));
 			  
 */
-import { getToDoData } from "./lib/firebase/api";
-import { toDoItemTemplate } from "./templates/toDoItem";
 
-
-async function appInit() {
-
-	const appData = await getToDoData();
-	// Interactive Templating
-	const toDoItems = Object.values(appData).map((obj) => {
-		// inside the object, access the "todo" field
-		return toDoItemTemplate(obj.todo);
-	});
-
-
-
-	//render function jsx +++ data
-	// template function() passing the data =====> template string html string
-
-	// interactive templating
-	// container
-	// batch update ... to dom updates all at once
-	// const div = document.createElement('div');
-
-	// toDoItems.forEach((markup => {
-	// 	div.appendChild(markup);
-	// }))
-
-	// document.querySelector("main").append(div);
-
-
-}
-
-appInit();
