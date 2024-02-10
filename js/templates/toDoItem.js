35b89dab3b476137ff97b7b470f5086b8d450a77
villaruzjoaquin <90798887+villaruzjoaquin@users.noreply.github.com>
2024-02-09 18:20:36 -0700

    making template look prettier

--- js/templates/toDoItem.js
+++ js/templates/toDoItem.js
@@ -12,14 +12,14 @@ component return jsx + data
 import { toElement } from "./../utils/toElement";
 
 function toDoItemTemplate(todo) {
-    const template =
-        `
-    <section class="bg-zinc-50 border-l-4 border-purple-600 shadow-sm rounded-xl p-4 md:p-5">
-        ${todo}
-      </section>
+    const template = `
+    <section class="bg-zinc-50 border-l-4 border-purple-600 shadow-sm rounded-xl p-4 md:p-6 lg:p-8 space-y-2 hover:bg-zinc-100 transition-colors duration-300 ease-in-out">
+        <p class="text-gray-800 text-sm md:text-base lg:text-lg font-medium whitespace-pre-line">${todo}</p>
+    </section>
     `;
 
     return toElement(template);
 }
+
 
 export { toDoItemTemplate };
