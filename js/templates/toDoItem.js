3bca5a8930e4d991f6c183dcf975f240423e2728
villaruzjoaquin <90798887+villaruzjoaquin@users.noreply.github.com>
2024-02-09 18:33:44 -0700

    Update toDoItem.js

--- js/templates/toDoItem.js
+++ js/templates/toDoItem.js
@@ -15,8 +15,11 @@ function toDoItemTemplate(todo) {
     const template = `
 
     <div class="grid grid-cols-12 gap-4">
-        <div class="col-span-4">${todo}</div>
+        <section class="bg-zinc-50 mb-2 border-l-4 border-purple-600 shadow-sm rounded-xl p-4 md:p-6 lg:p-8 space-y-2 hover:bg-zinc-100 transition-colors duration-300 ease-in-out">
+        <p class="text-gray-800 text-sm md:text-base lg:text-lg font-medium whitespace-pre-line">${todo}</p>
+        </section>  
     </div>
+
     `;
 
     return toElement(template);
