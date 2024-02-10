8e2b5041b99508a27b1cddc1902b1f388d91b1d1
villaruzjoaquin <90798887+villaruzjoaquin@users.noreply.github.com>
2024-02-09 18:35:59 -0700

    add card

--- js/templates/toDoItem.js
+++ js/templates/toDoItem.js
@@ -14,12 +14,20 @@ import { toElement } from "./../utils/toElement";
 function toDoItemTemplate(todo) {
     const template = `
 
-    <div class="grid grid-cols-12 gap-4">
-        <section class="bg-zinc-50 mb-2 border-l-4 border-purple-600 shadow-sm rounded-xl p-4 md:p-6 lg:p-8 space-y-2 hover:bg-zinc-100 transition-colors duration-300 ease-in-out">
-        <p class="text-gray-800 text-sm md:text-base lg:text-lg font-medium whitespace-pre-line">${todo}</p>
-        </section>  
-    </div>
-
+    <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+  <img class="w-full h-auto rounded-t-xl" src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80" alt="Image Description">
+  <div class="p-4 md:p-5">
+    <h3 class="text-lg font-bold text-gray-800 dark:text-white">
+      ${todo}
+    </h3>
+    <p class="mt-1 text-gray-500 dark:text-gray-400">
+      Some quick example text to build on the card title and make up the bulk of the card's content.
+    </p>
+    <a class="mt-2 py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
+      Go somewhere
+    </a>
+  </div>
+</div>
     `;
 
     return toElement(template);
