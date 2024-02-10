f8b77191f2c91f4f40bc4af6a1f425228bc233ea
villaruzjoaquin <90798887+villaruzjoaquin@users.noreply.github.com>
2024-02-09 18:32:47 -0700

    grid-test

--- js/templates/toDoItem.js
+++ js/templates/toDoItem.js
@@ -13,9 +13,10 @@ import { toElement } from "./../utils/toElement";
 
 function toDoItemTemplate(todo) {
     const template = `
-    <section class="bg-zinc-50 mb-2 border-l-4 border-purple-600 shadow-sm rounded-xl p-4 md:p-6 lg:p-8 space-y-2 hover:bg-zinc-100 transition-colors duration-300 ease-in-out">
-        <p class="text-gray-800 text-sm md:text-base lg:text-lg font-medium whitespace-pre-line">${todo}</p>
-    </section>
+
+    <div class="grid grid-cols-12 gap-4">
+        <div class="col-span-4">${todo}</div>
+    </div>
     `;
 
     return toElement(template);
