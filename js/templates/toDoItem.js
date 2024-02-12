a981f312a3d745231b6106c48f61702fe8d96c13
Joaquin Villaruz <onea75120@gmail.com>
2024-02-11 17:56:09 -0800

    added margin to each item

--- js/templates/toDoItem.js
+++ js/templates/toDoItem.js
@@ -14,8 +14,8 @@ import { toElement } from "./../utils/toElement";
 function toDoItemTemplate(todo) {
     const template = `
 
-    <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-        <div class="p-4 mb-5 md:p-5">
+    <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 mb-5 dark:shadow-slate-700/[.7]">
+        <div class="p-4 md:p-5">
             <h3 class="text-lg font-bold text-gray-800 dark:text-white">
             ${todo}
             </h3>
